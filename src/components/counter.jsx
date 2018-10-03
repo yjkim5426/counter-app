9c867a41a35ce1a9628810e2ee375e57e1301665
YJ-toolbox <youngjin@toolbox.net.nz>
2018-10-03 16:05:29 +1300

    Add a list

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,27 +2,23 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-      count: 0
+      count: 0,
+      tags: []
   };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+  }
 
   render() {
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        { this.state.tags.length === 0 && 'Please create a new tag!'}
+        { this.renderTags() }
       </div>
     );
-  }
-
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += (this.state.count === 0) ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-      const { count } = this.state;
-      return count === 0 ? 'Zero' : count;
   }
 }
 
